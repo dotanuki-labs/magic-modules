7d316c83454985bf932b605d1cb8e9b7a584da09
Ubiratan Soares <ubiratan.f.soares@gmail.com>
2020-04-17 20:20:04 +0200

    Added more errors

--- magic-modules-plugin/src/main/kotlin/io/labs/dotanuki/magicmodules/internal/model/MagicModulesError.kt
+++ magic-modules-plugin/src/main/kotlin/io/labs/dotanuki/magicmodules/internal/model/MagicModulesError.kt
@@ -5,4 +5,6 @@ internal sealed class MagicModulesError(description: String) : RuntimeException(
     object CantParseProjectStructure : MagicModulesError("Can't parse this project structure")
     object CantWriteConstantsFile : MagicModulesError("File should be a directory")
     object CantAcceptModulesNames : MagicModulesError("List of names can not be empty")
+    object MissingBuildSrc : MagicModulesError("This plugin requires a Gradle project with buildSrc")
+    object CantExtractGradleCoordinates : MagicModulesError("Something is wo")
 }
