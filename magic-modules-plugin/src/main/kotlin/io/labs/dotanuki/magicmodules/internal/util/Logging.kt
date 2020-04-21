cc870fbbede71213c0f500222e19cfba0cab878f
Ubiratan Soares <ubiratan.f.soares@gmail.com>
2020-04-21 14:34:30 +0200

    Logging with tags

--- magic-modules-plugin/src/main/kotlin/io/labs/dotanuki/magicmodules/internal/util/Logging.kt
+++ magic-modules-plugin/src/main/kotlin/io/labs/dotanuki/magicmodules/internal/util/Logging.kt
@@ -6,7 +6,8 @@ import org.gradle.api.logging.Logging
 
 internal inline fun <reified T> T.logger(): Logger = Logging.getLogger(T::class.java)
 
-internal fun Logger.i(message: String) = log(LogLevel.INFO, tagged(message))
-internal fun Logger.e(message: String) = log(LogLevel.ERROR, tagged(message))
+internal fun Logger.i(message: String) = log(LogLevel.INFO, tag(message))
+internal fun Logger.e(message: String) = log(LogLevel.ERROR, tag(message))
+internal fun Logger.l(message: String) = log(LogLevel.LIFECYCLE, tag(message))
 
-private fun tagged(message: String) = "[MagicModulesPlugin] $message"
+private fun tag(message: String) = "[MagicModulesPlugin] $message"
