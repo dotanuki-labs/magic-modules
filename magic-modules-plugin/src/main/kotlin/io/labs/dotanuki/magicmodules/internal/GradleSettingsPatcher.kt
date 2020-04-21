3ea9ec9c923f736561d15f0adbd6e190538e5ead
Ubiratan Soares <ubiratan.f.soares@gmail.com>
2020-04-21 14:33:48 +0200

    Mapping value in the functional pipeline

--- magic-modules-plugin/src/main/kotlin/io/labs/dotanuki/magicmodules/internal/GradleSettingsPatcher.kt
+++ magic-modules-plugin/src/main/kotlin/io/labs/dotanuki/magicmodules/internal/GradleSettingsPatcher.kt
@@ -2,13 +2,18 @@ package io.labs.dotanuki.magicmodules.internal
 
 import io.labs.dotanuki.magicmodules.internal.model.CanonicalModuleName
 import io.labs.dotanuki.magicmodules.internal.model.GradleModuleInclude
+import io.labs.dotanuki.magicmodules.internal.util.i
+import io.labs.dotanuki.magicmodules.internal.util.logger
 import org.gradle.api.initialization.Settings
 
 internal object GradleSettingsPatcher {
 
     fun patch(target: Settings, coordinates: Map<CanonicalModuleName, GradleModuleInclude>) {
-        coordinates.values.forEach { gradleInclude ->
-            target.include(gradleInclude.value)
-        }
+        coordinates.values
+            .map { it.value }
+            .forEach { gradleInclude ->
+                target.include(gradleInclude)
+                logger().i("Added to Settings -> $gradleInclude")
+            }
     }
 }
