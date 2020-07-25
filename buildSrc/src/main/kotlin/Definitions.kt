1c5c85e57d96f0fcf0f3ddf1dce46305abe5dc17
Ubiratan Soares <ubiratan.f.soares@gmail.com>
2020-07-25 11:50:01 -0300

    Release 0.0.2

--- buildSrc/src/main/kotlin/Definitions.kt
+++ buildSrc/src/main/kotlin/Definitions.kt
@@ -4,6 +4,6 @@
 
 object Definitions {
     const val groupId = "io.labs.dotanuki"
-    const val libraryVersion = "0.0.1"
+    const val libraryVersion = "0.0.2"
     const val targetJDK = "1.8"
 }
