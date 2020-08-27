f5c4826627e22fd763ffe4d7e28757191e3e1234
Thiago Santos <programadorthi@gmail.com>
2020-08-27 15:58:40 -0300

    fixed using unix separator only that not works when running with Windows SO

--- magic-modules-plugin/src/test/kotlin/io/labs/dotanuki/magicmodules/tests/internal/ExtractCoordinatesTests.kt
+++ magic-modules-plugin/src/test/kotlin/io/labs/dotanuki/magicmodules/tests/internal/ExtractCoordinatesTests.kt
@@ -1,22 +1,23 @@
 package io.labs.dotanuki.magicmodules.tests.internal
 
+import io.labs.dotanuki.magicmodules.internal.MagicModulesError
 import io.labs.dotanuki.magicmodules.internal.model.CanonicalModuleName
 import io.labs.dotanuki.magicmodules.internal.model.GradleBuildScript
 import io.labs.dotanuki.magicmodules.internal.model.GradleModuleInclude
 import io.labs.dotanuki.magicmodules.internal.model.GradleModuleType
-import io.labs.dotanuki.magicmodules.internal.MagicModulesError
 import io.labs.dotanuki.magicmodules.internal.util.ExtractCoordinates
 import org.assertj.core.api.Assertions.assertThat
 import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.assertj.core.api.ThrowableAssert.ThrowingCallable
 import org.junit.Test
+import java.io.File
 
 class ExtractCoordinatesTests {
 
     @Test fun `should throw when coordinates extraction not possible`() {
 
         val projectName = "friendsfinder"
-        val scriptFilePath = "/home/Dev/blogger/app/build.gradle"
+        val scriptFilePath = "${File.separator}home${File.separator}Dev${File.separator}blogger${File.separator}app${File.separator}build.gradle"
         val buildScript = GradleBuildScript(scriptFilePath, GradleModuleType.APPLICATION)
 
         val execution = ThrowingCallable { ExtractCoordinates.gradleInclude(projectName, buildScript) }
@@ -26,7 +27,7 @@ class ExtractCoordinatesTests {
 
     @Test fun `should extract coordinates with one level`() {
         val projectName = "awesome"
-        val scriptFilePath = "/home/AndroidStudioProjects/awesome/app/build.gradle"
+        val scriptFilePath = "${File.separator}home${File.separator}AndroidStudioProjects${File.separator}awesome${File.separator}app${File.separator}build.gradle"
         val buildScript = GradleBuildScript(scriptFilePath, GradleModuleType.APPLICATION)
 
         val extractedInclude = ExtractCoordinates.gradleInclude(projectName, buildScript)
@@ -42,7 +43,7 @@ class ExtractCoordinatesTests {
     @Test fun `should extract gradle module with two levels`() {
 
         val projectName = "twitter-clone"
-        val scriptFilePath = "/Desktop/twitter-clone/features/login/build.gradle.kts"
+        val scriptFilePath = "${File.separator}Desktop${File.separator}twitter-clone${File.separator}features${File.separator}login${File.separator}build.gradle.kts"
         val buildScript = GradleBuildScript(scriptFilePath, GradleModuleType.LIBRARY)
 
         val extractedInclude = ExtractCoordinates.gradleInclude(projectName, buildScript)
@@ -58,7 +59,7 @@ class ExtractCoordinatesTests {
     @Test fun `should extract gradle module with three levels`() {
 
         val projectName = "coronatracker"
-        val scriptFilePath = "/Desktop/coronatracker/features/login/recover/build.gradle.kts"
+        val scriptFilePath = "${File.separator}Desktop${File.separator}coronatracker${File.separator}features${File.separator}login${File.separator}recover${File.separator}build.gradle.kts"
         val buildScript = GradleBuildScript(scriptFilePath, GradleModuleType.LIBRARY)
 
         val extractedInclude = ExtractCoordinates.gradleInclude(projectName, buildScript)
